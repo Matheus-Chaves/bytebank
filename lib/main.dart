0f42c0dadbd505d1a58288e9af4c275dc2bbeefb
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-05-04 16:51:22 -0300

    refatorando código e aumentando legibilidade

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp(const MyApp());
   runApp(
-    Column(
-      children: <Widget>[
-        const Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
+    MaterialApp(
+      home: Scaffold(
+        body: const ListaTransferencias(),
+        appBar: AppBar(title: const Text("Transferências")),
+        floatingActionButton: const FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: null,
         ),
-        const Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        const Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children: const <Widget>[
-            Text(
-              'Deliver features faster',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        ),
-      ],
+      ),
     ),
+  );
+}
+
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.56, 1000, "05/2022")),
+        ItemTransferencia(Transferencia(1009, 1000, "06/2022")),
+        ItemTransferencia(Transferencia(5423, 1000, "07/2022")),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(
+    this._transferencia,
+  ) : super(key: null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(_transferencia.valorTransferencia.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+        trailing: Text(_transferencia.dataTransferencia),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valorTransferencia;
+  final int numeroConta;
+  final String dataTransferencia;
+
+  Transferencia(
+    this.valorTransferencia,
+    this.numeroConta,
+    this.dataTransferencia,
   );
 }
