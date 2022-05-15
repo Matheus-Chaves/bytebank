0f3b3b51279e7c5481b34d46b02f7c23bb07c9e4
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-05-15 17:50:53 -0300

    refatorando dashboard e adicionando lints

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const BytebankApp());
@@ -22,45 +23,6 @@ class BytebankApp extends StatelessWidget {
         ),
       ),
       home: const Dashboard(),
-    );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-  const Dashboard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  Icon(Icons.people, color: Colors.white, size: 32),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
