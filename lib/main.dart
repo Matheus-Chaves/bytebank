ba473694da8b503705c990d25a5bd49a1f0cb7d2
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-05-15 23:24:38 -0300

    implementando sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'database/app_database.dart';
+import 'models/contact.dart';
 import 'screens/contacts_list.dart';
 import 'screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const BytebankApp());
+void main() {
+  runApp(const BytebankApp());
+  save(Contact(0, "Matheus", 9000)).then((id) {
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
+}
 
 class BytebankApp extends StatelessWidget {
   const BytebankApp({Key? key}) : super(key: key);
