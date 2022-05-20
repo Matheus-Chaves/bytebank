06c33a3bec3aca632c7b5e5734b7b5b4b777fde6
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-05-19 23:19:44 -0300

    implementando monitoramento da requisição http

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    debugPrint('Request');
+    debugPrint('url: ${data.url}');
+    debugPrint('headers: ${data.headers}');
+    debugPrint('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    debugPrint('Response');
+    debugPrint('status code: ${data.statusCode}');
+    debugPrint('headers: ${data.headers}');
+    debugPrint('body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
+  final Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+  );
   final Response response =
-      await get(Uri.http('192.168.0.16:8080', 'transactions'));
-  debugPrint(response.body);
+      await client.get(Uri.http('192.168.0.16:8080', 'transactions'));
 }
