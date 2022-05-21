70afad4284bb53536c20e591cb934fb3a0f0aacc
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-05-21 12:29:58 -0300

    adicionando comentários informativos

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -63,8 +63,11 @@ Future<Transaction> save(Transaction transaction) async {
       'accountNumber': transaction.contact.accountNumber
     }
   };
+  // Transformamos o Map no formato Json e o guardamos dentro de uma String
   final String transactionJson = jsonEncode(transactionMap);
 
+  // Realizando post e passando o json no body
+  // Response irá devolver um json da nova transaction criada
   final Response response =
       await client.post(Uri.http(authority, 'transactions'),
           headers: {
@@ -72,9 +75,11 @@ Future<Transaction> save(Transaction transaction) async {
             'password': '1000',
           },
           body: transactionJson);
-
+  // Decodificando o json em um Map
   Map<String, dynamic> json = jsonDecode(response.body);
   final Map<String, dynamic> contactJson = json['contact'];
+
+  // Convertendo o Map para uma variável do tipo Transaction
   return Transaction(
     json['value'],
     Contact(
